d8a370bc028b866edab091d4d845efd57e909ffa
Ayse <ayse@example.com>
2026-01-22 17:46:24 +0000

    Fix FizzBuzz: remove trailing space for autograder

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+import sys
 
 def fizzbuzz(n):
     for i in range(1, n + 1):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
+            print("FizzBuzz", end="" if i == n else " ")
         elif i % 3 == 0:
-            print("Fizz", end=" ")
+            print("Fizz", end="" if i == n else " ")
         elif i % 5 == 0:
-            print("Buzz", end=" ")
+            print("Buzz", end="" if i == n else " ")
         else:
-            print(i, end=" ")
+            print(i, end="" if i == n else " ")
     print()
 
 if __name__ == "__main__":
-    import sys
-    fizzbuzz(int(sys.argv[1]))
+    if len(sys.argv) != 2:
+        print("Usage: ./0-fizzbuzz.py <number>")
+    else:
+        fizzbuzz(int(sys.argv[1]))
