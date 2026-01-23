9ba238e5ef8ee518c9adef0d4e4d1cd77866ba86
Ayse <ayse@example.com>
2026-01-23 05:03:45 +0000

    Fix delete_dnodeint_at_index

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,26 +1,43 @@
 #include "lists.h"
+#include <stdlib.h>
 
-/* Delete node at index */
+/**
+ * delete_dnodeint_at_index - silir node-u double linked list-dən
+ * @head: pointer pointer-ə list-in başına
+ * @index: silinəcək node-un index-i (0-dan başlayır)
+ * Return: 1 uğurlu silmə, 0 uğursuz
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
     dlistint_t *current;
     unsigned int i;
 
     if (!head || !*head)
-        return (-1);
+        return (0);
 
     current = *head;
 
-    for (i = 0; i < index && current; i++)
+    /* index 0-dırsa, baş node-u silirik */
+    if (index == 0)
+    {
+        *head = current->next;        /* başı növbəti node-a dəyişirik */
+        if (*head)
+            (*head)->prev = NULL;     /* yeni başın prev-i NULL olur */
+        free(current);
+        return (1);
+    }
+
+    /* index-i tapmaq üçün loop */
+    for (i = 0; current && i < index; i++)
         current = current->next;
 
+    /* əgər index çox böyükdür */
     if (!current)
-        return (-1);
+        return (0);
 
+    /* node-u list-dən ayırırıq */
     if (current->prev)
         current->prev->next = current->next;
-    else
-        *head = current->next; /* deleting head */
 
     if (current->next)
         current->next->prev = current->prev;
